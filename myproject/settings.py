8a6d15fadf2623a5ce5b1b2b14db5f7a4f9599f7
wulf <ppkill00@gmail.com>
2018-11-22 12:35:07 +0900

    allow host

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '-^z#*=34ps1me702pg_&mjp)@h0@1-8$o008yt2jw42ot7*&#@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
